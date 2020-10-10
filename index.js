e2e9af2d3f03b6c33e60d6c35c4b56f7f8d0eae8
Tim Anido <xanido@gmail.com>
2020-10-10 17:04:01 +1100

    add more inputs

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const github = require('@actions/github');
 const core = require('@actions/core');
 
+const name = core.getInput('name');
+const content = core.getInput('content');
 const TOKEN = core.getInput('token') || process.env.CF_API_TOKEN;
 const workspacePath = github.context.workspacePath || process.env.GITHUB_WORKSPACE;
 
@@ -11,15 +13,23 @@ const cf = require('cloudflare')({
     token: TOKEN
 });
 
-async function main() {
+// this action is specific to my needs.. if no `name` input is supplied
+// then the action tries to determine it by parsing docker-compose.yml
+// traefik labels.
+const extractTraefikHostname = () => {
     const dockerComposeDefinition = fs.readFileSync(path.join(workspacePath, 'docker-compose.yml')).toString();
-    
-    const [,hostname] = dockerComposeDefinition.match(/Host\(`([^`]+)`\)/) || [];
-    
+    const [, hostname] = dockerComposeDefinition.match(/Host\(`([^`]+)`\)/) || [];
+    return hostname;
+}
+
+async function main() {
+    const hostname = name || extractTraefikHostname();
     const hostparts = hostname.split('.');
     let zoneId;
     let cursor = hostparts.length;
     let zoneName;
+
+    // pop segments from the name until we find a matching zone
     while (!zoneId && cursor > 0) {
         zoneName = hostparts.slice(0 - cursor).join('.');
         let zoneDetails;
@@ -41,25 +51,21 @@ async function main() {
 
     // check to see if the dns record already exists
     const dnsRecordResult = await cf.dnsRecords.browse(zoneId, {name: hostname});
+    const dnsParams = {
+        type: "CNAME",
+        name: dnsName,
+        content,
+        ttl: 1, // 'automatic'
+        proxied: true,
+    };
+        
     if(dnsRecordResult.result && dnsRecordResult.result.length) {
         console.log('record found, updating...')
-        const recordCreationResult = await cf.dnsRecords.edit(zoneId, dnsRecordResult.result[0].id, {
-            type: "CNAME",
-            name: dnsName,
-            content: "odin.xanido.net",
-            ttl: 1, // 'automatic'
-            proxied: true,
-        });
+        const recordCreationResult = await cf.dnsRecords.edit(zoneId, dnsRecordResult.result[0].id, dnsParams);
         console.log(`updated record with id: ${dnsRecordResult.result[0].id}`);
     } else {
         console.log('record not found, creating...')
-        const recordCreationResult = await cf.dnsRecords.add(zoneId, {
-            type: "CNAME",
-            name: dnsName,
-            content: "odin.xanido.net",
-            ttl: 1, // 'automatic'
-            proxied: true,
-        });
+        const recordCreationResult = await cf.dnsRecords.add(zoneId, dnsParams);
         console.log(`created record with id: ${recordCreationResult.result.id}`);
     }
 }
